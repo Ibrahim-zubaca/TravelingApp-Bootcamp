0cd734ba490a99659b8d7ae59570622ef14b6c15
Ibrahim-zubaca <zubaca.ibrahim@gmail.com>
2024-06-28 09:08:04 +0200

    Initial Commit

---
+++ TravelingApp-Bootcamp/TravelingApp_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelingApp_BootcampApp.swift
+//  TravelingApp-Bootcamp
+//
+//  Created by Purple Key on 28. 6. 2024..
+//
+
+import SwiftUI
+
+@main
+struct TravelingApp_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
