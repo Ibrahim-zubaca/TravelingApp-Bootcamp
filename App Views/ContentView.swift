53ca7698dc09af1a158a44c7cc492e67efdb9ecb
Ibrahim-zubaca <zubaca.ibrahim@gmail.com>
2024-06-28 11:08:05 +0200

    Home Screen Layout - ForEach Hashable and System Images

--- App Views/ContentView.swift
+++ App Views/ContentView.swift
@@ -12,10 +12,10 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 DiscoverCategoryView()
-                
-                
-                
-            }
+                PopularDestinationView()
+                PopularRestaurantsView()
+                TrendingCreatorsView()
+                }
             .navigationTitle("Discover")
         }
     }
@@ -32,24 +32,147 @@ struct ContentView_Previews: PreviewProvider {
 
 //EXTRACTED VIEWS AND COMPONENTS
 //CATEGORY SECTION ----------------------------------
+struct Category: Hashable {
+    let name, imageName: String
+}
+
 struct DiscoverCategoryView: View {
+    
+    let categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live Envents", imageName: "music.mic"),
+        .init(name: "Food", imageName: "fork.knife"),
+        .init(name: "History", imageName: "building.columns")
+    ]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 8){
-                ForEach(0..<16, id: \.self) { num in
+            HStack(spacing: 16){
+                ForEach(categories, id: \.self) { category in
                     VStack {
-                        Spacer()
-                            .frame(width: 60, height: 60)
-                            .background(.gray)
-                            .cornerRadius(.infinity)
-                        Text("Art")
-                            .font(.caption)
+                        Image(systemName: category.imageName)
+                            .foregroundColor(.white)
+                            .font(.system(size: 25))
+                            .frame(width:70, height: 70)
+                            .background(Color.gray)
+                            .cornerRadius(50)
+                            .shadow(color: .gray, radius: 4, x:0, y:2)
+                        Text(category.name)
+                            .font(.subheadline)
                             .fontWeight(.semibold)
                             .padding(.top, 5)
                     }
                 }
             }
             .padding(.horizontal)
+            .padding(.top)
         }
     }
 }
+
+
+
+//POPULAR DESTINATION SECTION ----------------------------------
+struct PopularDestinationView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Popular destination")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                Spacer()
+                Text("See all")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+            }
+            .padding(.horizontal)
+            .padding(.top,30)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack {
+                    ForEach(0..<5, id: \.self) { num in
+                        Rectangle()
+                            .frame(width: 150, height: 180)
+                            .cornerRadius(10)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.horizontal)
+                
+            }
+        }
+    }
+}
+
+//POPULAR PLACES TO EAT SECTION ----------------------------------
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Popular places to eat")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                Spacer()
+                Text("See all")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+            }
+            .padding(.horizontal)
+            .padding(.top,30)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack {
+                    ForEach(0..<5, id: \.self) { num in
+                        Rectangle()
+                            .frame(width: 200, height:64)
+                            .cornerRadius(10)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.horizontal)
+                
+            }
+        }
+    }
+}
+
+//TRENDING CREATORS SECTION ----------------------------------
+struct TrendingCreatorsView: View {
+    var body: some View {
+        
+            VStack{
+                HStack{
+                    Text("Popular Creators")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Text("See all")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+                .padding(.top,30)
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(spacing: 8){
+                        ForEach(0..<16, id: \.self) { num in
+                            VStack {
+                                Spacer()
+                                    .frame(width: 60, height: 60)
+                                    .background(.gray)
+                                    .cornerRadius(.infinity)
+                                Text("Art")
+                                    .font(.caption)
+                                    .fontWeight(.semibold)
+                                    .padding(.top, 5)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.top)
+                    
+                }
+            }
+        }
+    }
